c5d5c02086cf27c803fb39eebee92e673ee49ebf
James Cornelius <jaymcorn@gmail.com>
2019-10-22 19:59:02 -0400

    Created grid layout for social card

--- src/components/SocialCard.jsx
+++ src/components/SocialCard.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import Header from './Header';
+// eslint-disable-next-line import/no-unresolved
+import OhWonder from '../assets/images/ohWonder.jpeg';
 import './SocialCard.css';
 
+// eslint-disable-next-line react/prefer-stateless-function
 class SocialCard extends React.Component {
-    render() {
-        return (
-            <div className="Social-Card">
-                <Header name="James Cornelius" userName="BamBam" />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="Social-Card">
+        <Header name="James Cornelius" userName="BamBam" />
+        <div className="Social-Card-Caption">
+            <img src={OhWonder} alt="Oh Wonder" className="Social-Card-Caption-Image" />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default SocialCard;
